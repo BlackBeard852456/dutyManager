44d9cf52f669a95d3cb861bdc78b86c3d6bf33d5
BlackBeard852456 <black.barbe147@gmail.com>
2021-11-22 14:58:38 +0100

    Ajout de la fonction qui créer un nouveau devoir dans duty.go

--- src/duty/duty.go
+++ src/duty/duty.go
@@ -14,8 +14,13 @@ type Duty struct {
 	matter   string
 }
 
-func CreateNewDuty() {
+func CreateNewDuty() Duty {
 	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Entre le nom du devoir : ")
 	dutyName, _ := reader.ReadString('\n')
-	fmt.Println(dutyName)
+	fmt.Print("Entre l'intitulé du devoir : ")
+	dutyEntilted, _ := reader.ReadString('\n')
+	fmt.Print("Entre la matière du devoir : ")
+	dutyMatter, _ := reader.ReadString('\n')
+	return Duty{name: dutyName, entilted: dutyEntilted, matter: dutyMatter}
 }
