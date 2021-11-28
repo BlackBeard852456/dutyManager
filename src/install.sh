0acbfa1552d7fdc803deabdf246f4963c893a199
BlackBeard852456 <black.barbe147@gmail.com>
2021-11-28 15:46:16 +0100

    Amélioration du script d'installation

--- src/install.sh
+++ src/install.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+
+##########################
+
+# SCRIPT D'INSTALLATION
+
+#########################
+
+
+username=$(whoami)
+
 echo "Installation du programme" 
 mkdir $HOME/.dutyManager
 sudo go build -o dutyManager main.go
@@ -7,3 +17,6 @@ sudo mv ./dutyManager $HOME/.dutyManager/
 sudo mv db.db $HOME/.dutyManager/
 sudo echo "export PATH=$PATH:$HOME/.dutyManager/" > $HOME/.profile
 sudo echo $HOME/.profile
+sudo chown $username:$username $HOME/.dutyManager/db.db 
+sudo chown $username:$username $HOME/.dutyManager/dutyManager
+
